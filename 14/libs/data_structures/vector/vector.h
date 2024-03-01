936d5a7987118b76b88bccf6bd38d18e29d428a1
Ann232 <scorpiusanna@gmail.com>
2024-03-01 17:31:19 +0300

    commit

--- 14/libs/data_structures/vector/vector.h
+++ 14/libs/data_structures/vector/vector.h
@@ -1,4 +1,12 @@
 #ifndef INC_14_VECTOR_H
 #define INC_14_VECTOR_H
 
+#include <stdio.h>
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
 #endif //INC_14_VECTOR_H
