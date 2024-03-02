540073f7e60be1630b3cf8307125ba70e6184d89
Ann232 <scorpiusanna@gmail.com>
2024-03-02 14:46:04 +0300

    memory usage of vector

--- 14/libs/data_structures/vector/vector.c
+++ 14/libs/data_structures/vector/vector.c
@@ -1,2 +1,86 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "vector.h"
 
+
+// возвращает структуру-дескриптор вектор из n значений
+vector createVector(size_t n)
+{
+    int* data = malloc(n * sizeof(int));
+
+    if (data)
+        return (vector) {data, 0, n};
+
+    fprintf(stderr, "bad alloc");
+    exit(1);
+}
+
+
+// изменяет количество
+// памяти, выделенное под хранение элементов вектора
+void reserve(vector* v, size_t new_capacity)
+{
+    if (v->capacity != 0 && new_capacity == 0)
+    {
+        free(v->data);
+        v->data = NULL;
+        v->size = 0;
+        v->capacity = 0;
+    }
+    else if (new_capacity != 0)
+    {
+        if (v->capacity == 0)
+        {
+            int* data = malloc(new_capacity * sizeof(int));
+
+            if (data)
+            {
+                v->data = data;
+                v->size = new_capacity;
+                v->capacity = new_capacity;
+            }
+            else
+            {
+                fprintf(stderr, "bad alloc");
+                exit(1);
+            }
+        }
+        else
+        {
+            int* data = realloc(v->data, new_capacity);
+
+            if (data)
+            {
+                v->data = data;
+                v->size = new_capacity;
+                v->capacity = new_capacity;
+            }
+            else
+            {
+                fprintf(stderr, "bad alloc");
+                exit(1);
+            }
+        }
+    }
+}
+
+
+// удаляет элементы из контейнера, но не освобождает выделенную память
+void clear(vector* v)
+{
+    v->size = 0;
+}
+
+
+// освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFit(vector* v)
+{
+    v->capacity = v->size;
+}
+
+
+//  освобождает память, выделенную вектору
+void deleteVector(vector* v)
+{
+    free(v->data);
+}
--- 14/libs/data_structures/vector/vector.h
+++ 14/libs/data_structures/vector/vector.h
@@ -3,10 +3,28 @@
 
 #include <stdio.h>
 
-typedef struct vector {
+typedef struct vector
+{
     int *data; // указатель на элементы вектора
     size_t size; // размер вектора
     size_t capacity; // вместимость вектора
 } vector;
+
+
+// возвращает структуру-дескриптор вектор из n значений
+vector createVector(size_t);
+
+// изменяет количество
+// памяти, выделенное под хранение элементов вектора
+void reserve(vector*, size_t);
+
+// удаляет элементы из контейнера, но не освобождает выделенную память
+void clear(vector*);
+
+// освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFit(vector*);
+
+//  освобождает память, выделенную вектору
+void deleteVector(vector*);
 
 #endif //INC_14_VECTOR_H
